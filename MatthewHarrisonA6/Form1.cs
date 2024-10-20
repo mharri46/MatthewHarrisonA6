ca9c3a1bbec1ab452b5b4bb29e2623ee2abdfe4b
Matthew Harrison <mharri46@my.westga.edu>
2024-10-20 19:54:03 -0400

    Add project files.

---
+++ MatthewHarrisonA6/Form1.cs
@@ -0,0 +1,15 @@
+namespace MatthewHarrisonA6
+{
+    public partial class Assignment6Form : Form
+    {
+        public Assignment6Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
